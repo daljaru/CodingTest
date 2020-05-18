a9aa22135e641ce913f549ce552f1d5dfe1a875e
daljaru <chocolate_cake@kakao.com>
2020-05-18 13:00:58 +0900

    add additional code

--- course1/01_sum_betwAB.py
+++ course1/01_sum_betwAB.py
@@ -22,3 +22,20 @@ def solution(a, b):
 if __name__ == '__main__':
     answer = solution(5, 3)
     print(answer)
+
+
+
+# 다른 좋은 풀이들...  그냥 sum을 쓰면 되는 것이였다.
+ def adder(a, b):
+    # 함수를 완성하세요
+    if a > b: a, b = b, a
+
+    return sum(range(a,b+1))   
+
+
+def adder(a, b):
+    # 함수를 완성하세요
+    return sum(range(min(a,b),max(a,b)+1))
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print( adder(3, 5))
